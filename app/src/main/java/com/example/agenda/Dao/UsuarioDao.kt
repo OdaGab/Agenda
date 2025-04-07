cdf77dc63ee9da60099adbfa2d8ea85638a36940
odair gabriel <odair.silva@salas.aulas>
2025-04-07 09:14:46 -0300

    configurando o insert no DAO

---
+++ app/src/main/java/com/example/agenda/Dao/UsuarioDao.kt
@@ -0,0 +1,11 @@
+package com.example.agenda.Dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+
+@Dao
+interface UsuarioDao {
+    @Insert
+
+    fun inserir(listaUsuario: MutableList<Usuario>)
+}
